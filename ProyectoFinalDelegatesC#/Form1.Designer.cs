8fb232e69b0273468c85a778f34c72c2294480b7
Smorareq1 <smorareq1@gmail.com>
2024-02-15 20:03:20 -0600

    PonerGestorDeUsuariosEnProceso

--- ProyectoFinalDelegatesC#/Form1.Designer.cs
+++ ProyectoFinalDelegatesC#/Form1.Designer.cs
@@ -31,115 +31,77 @@
             textBox1 = new TextBox();
             label1 = new Label();
             label2 = new Label();
-            label3 = new Label();
             label4 = new Label();
-            label5 = new Label();
             button1 = new Button();
-            textBox2 = new TextBox();
             textBox3 = new TextBox();
-            textBox4 = new TextBox();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(581, 143);
+            textBox1.Location = new Point(517, 144);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(110, 23);
             textBox1.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(336, 43);
+            label1.Location = new Point(294, 32);
             label1.Name = "label1";
-            label1.Size = new Size(241, 35);
+            label1.Size = new Size(182, 28);
             label1.TabIndex = 1;
             label1.Text = "CHEPE TE VENDE";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(241, 146);
+            label2.Location = new Point(210, 147);
             label2.Name = "label2";
-            label2.Size = new Size(59, 20);
+            label2.Size = new Size(47, 15);
             label2.TabIndex = 2;
             label2.Text = "Usuario";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(240, 202);
-            label3.Name = "label3";
-            label3.Size = new Size(54, 20);
-            label3.TabIndex = 3;
-            label3.Text = "Correo";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(240, 254);
+            label4.Location = new Point(210, 190);
             label4.Name = "label4";
-            label4.Size = new Size(83, 20);
+            label4.Size = new Size(67, 15);
             label4.TabIndex = 4;
             label4.Text = "Contraseña";
             // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(239, 308);
-            label5.Name = "label5";
-            label5.Size = new Size(137, 20);
-            label5.TabIndex = 5;
-            label5.Text = "Verficar Contraseña";
-            // 
             // button1
             // 
-            button1.Location = new Point(369, 418);
+            button1.Location = new Point(315, 252);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(184, 29);
+            button1.Size = new Size(161, 22);
             button1.TabIndex = 6;
             button1.Text = "Crear Usuario";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(581, 202);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 7;
-            // 
             // textBox3
             // 
-            textBox3.Location = new Point(581, 254);
+            textBox3.Location = new Point(508, 190);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(125, 27);
+            textBox3.Size = new Size(110, 23);
             textBox3.TabIndex = 8;
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(581, 308);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(125, 27);
-            textBox4.TabIndex = 9;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
-            Controls.Add(textBox4);
+            ClientSize = new Size(800, 450);
             Controls.Add(textBox3);
-            Controls.Add(textBox2);
             Controls.Add(button1);
-            Controls.Add(label5);
             Controls.Add(label4);
-            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(textBox1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -152,12 +114,8 @@
         private TextBox textBox1;
         private Label label1;
         private Label label2;
-        private Label label3;
         private Label label4;
-        private Label label5;
         private Button button1;
-        private TextBox textBox2;
         private TextBox textBox3;
-        private TextBox textBox4;
     }
 }
--- ProyectoFinalDelegatesC#/Form1.cs
+++ ProyectoFinalDelegatesC#/Form1.cs
@@ -14,20 +14,19 @@ namespace ProyectoFinalDelegatesC_
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Usuario usuario1 = new Usuario(textBox1.Text.ToString(), textBox2.Text.ToString(), textBox3.Text.ToString());
-            string Contrasena1 = textBox3.Text.ToString();
-            string Contrasena2 = textBox4.Text.ToString();
+            
 
-            if (Contrasena1 != Contrasena2)
-            {
-                MessageBox.Show("Las contraseñas no coinciden");
-            }
-            else{
+
+                string Contrasena1 = textBox3.Text.ToString();
+            
+
+            
+           
                 this.Hide();
                 Iniciar iniciar = new Iniciar();
                 iniciar.SetInformacion(usuario1.Nombre, usuario1.Correo);
                 iniciar.Show();
-            }
+            
     
         }
     }
