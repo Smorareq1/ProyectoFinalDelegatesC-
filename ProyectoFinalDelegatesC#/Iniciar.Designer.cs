755b8ede3dad977af043e226e55f49e0ddf796f1
Smorareq1 <smorareq1@gmail.com>
2024-02-15 18:41:44 -0600

    SupuestoCargarDatosCompletado

--- ProyectoFinalDelegatesC#/Iniciar.Designer.cs
+++ ProyectoFinalDelegatesC#/Iniciar.Designer.cs
@@ -30,33 +30,51 @@
         {
             label1 = new Label();
             nombreUsuario = new Label();
+            button1 = new Button();
+            openFileDialog1 = new OpenFileDialog();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(3, 15);
+            label1.Location = new Point(3, 11);
             label1.Name = "label1";
-            label1.Size = new Size(90, 20);
+            label1.Size = new Size(72, 15);
             label1.TabIndex = 0;
             label1.Text = "Bienvenido: ";
             // 
             // nombreUsuario
             // 
             nombreUsuario.AutoSize = true;
-            nombreUsuario.Location = new Point(87, 15);
+            nombreUsuario.Location = new Point(76, 11);
             nombreUsuario.Name = "nombreUsuario";
-            nombreUsuario.Size = new Size(50, 20);
+            nombreUsuario.Size = new Size(38, 15);
             nombreUsuario.TabIndex = 1;
             nombreUsuario.Text = "label2";
             // 
+            // button1
+            // 
+            button1.Location = new Point(295, 99);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
             // Iniciar
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(button1);
             Controls.Add(nombreUsuario);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Iniciar";
             Text = "Iniciar";
             Load += Iniciar_Load;
@@ -68,5 +86,7 @@
 
         private Label label1;
         private Label nombreUsuario;
+        private Button button1;
+        private OpenFileDialog openFileDialog1;
     }
 }
--- ProyectoFinalDelegatesC#/Iniciar.cs
+++ ProyectoFinalDelegatesC#/Iniciar.cs
@@ -12,9 +12,12 @@ namespace ProyectoFinalDelegatesC_
 {
     public partial class Iniciar : Form
     {
-
+        //Variables 
         string Nombre;
         string Correo;
+        string filePath;
+
+
         public Iniciar()
         {
             InitializeComponent();
@@ -29,7 +32,31 @@ namespace ProyectoFinalDelegatesC_
         {
             Nombre = nombre;
             Correo = correo;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Crear un objeto de OpenFileDialog
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Configurar propiedades del diálogo
+            openFileDialog1.InitialDirectory = "c:\\"; 
+            openFileDialog1.Filter = "Archivos de texto (*.txt)|*.txt|Archivos CSV (*.csv)|*.csv"; 
+            openFileDialog1.FilterIndex = 1; // Índice del filtro predeterminado
+
             
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Obtener la ruta del archivo seleccionado
+                filePath = openFileDialog1.FileName;
+                MessageBox.Show("Ruta del archivo seleccionado: " + filePath);
+            }
+
+            Producto.LeerArchivo(filePath);
+            MessageBox.Show("Archivo leído con éxito");
+
+
         }
     }
 }
